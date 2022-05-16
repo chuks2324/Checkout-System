20157840d3446a3feb906b6413c2f6f43797a794
chuks2324 <54580449+chuks2324@users.noreply.github.com>
2022-05-16 20:30:18 +0100

    Contains Item Class
    When Initialised creates an object of type Item with three variables (item name, price, and barcode).
    All three variables are initialized with user input.

---
+++ items.py
@@ -0,0 +1,27 @@
+class items:
+    def __init__(self, _name, _price, _barcode):
+        self.price = _price
+        self.name = _name
+        self.barcode = _barcode
+
+    def getprice(self):
+        return self.price
+
+    def getname(self):
+        return self.name
+    
+    def getbarcode(self):
+        return self.barcode
+    
+    def changeprice(self, newprice):
+        self.price = newprice
+
+    def changebarcode(self, newbarcode):
+        self.barcode = newbarcode
+
+    def changename(self, newname):
+        self.name = newname
+    
+    def iteminfo(self):
+        info = "Item name is", self.name,"/n Price is", self.price, "/n Barcode is", self.barcode,"/n"
+        return info
